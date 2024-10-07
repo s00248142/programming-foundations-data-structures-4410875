29ca5d4b1101587d115e3d78bbd41a44ef2ef0d6
s00248142 <s00248142@atu.ie>
2024-10-07 22:02:58 +0100

    Finished section 4

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -5,10 +5,21 @@ user_preferences = {
     "currency": "USD",
     "theme": None
 }
-
+print("\n")
+print("Original Dictionary:")
+print(user_preferences)
 
 def update_preferences(user_pref):
-    return {}
+    new_user_preferences = {}
+    for key in user_pref:
+    #for key, value in user_pref.items():     
+        if user_pref[key] != None:
+        #if value is not None:
+            new_user_preferences[key] = user_pref[key]
+            
+    return new_user_preferences
 
-
+print("\n")
+print("Updated Dictionary:")
 print(update_preferences(user_preferences))
+print("\n")
